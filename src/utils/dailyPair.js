c986bd70256343d453f19415500a97e7f6640262
Mike Weatherford <mikesweatherford@gmail.com>
2025-06-01 14:16:58 -0400

    edited tomorrows daily

--- src/utils/dailyPair.js
+++ src/utils/dailyPair.js
@@ -3,7 +3,7 @@ export function getTodayChain() {
   const targets = {
     '2025-05-31': ['The_Boat_Race_2018', 'Melon'],
     '2025-06-01': ['Viagra_Boys', 'I_Think_You_Should_Leave_with_Tim_Robinson', 'Mammal', 'Jurassic'],
-    '2025-06-02': ['Basketball', 'Milwaukee', 'Arcade_cabinet', 'American_Airlines_Flight_1420'],
+    '2025-06-02': ['Basketball', 'Milwaukee', 'Arcade_cabinet', 'Airplane'],
   };
   return targets[today] || ['Wikipedia', 'Sorry'];
 }
