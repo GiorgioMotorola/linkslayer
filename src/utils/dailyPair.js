634eee48386447e363352cbc10e31b0638cf6166
Mike Weatherford <mikesweatherford@gmail.com>
2025-06-15 22:48:15 -0400

    updated game

--- src/utils/dailyPair.js
+++ src/utils/dailyPair.js
@@ -17,7 +17,7 @@ export function getTodayChain() {
     '2025-06-13': ['Response_to_sneezing', 'Universal_Studios_Florida', 'Water'],
     '2025-06-14': ['Kentucky_Bend', 'Antarctica', 'Guitar'],
     '2025-06-15': ['Kithara', 'Golf', 'Gasoline'],
-    '2025-06-16': ['Action_Park', 'Gate', 'Lottery'],
+    '2025-06-16': ['Action_Park', 'Cat', 'Lottery'],
     '2025-06-17': ['Avril_14th', 'Dog', 'Backpack'],
     '2025-06-18': ['The_Apartment', 'Landlord', 'Ghost'],
     '2025-06-19': ['Nathan_for_You', 'Telephone', 'Sun'],
