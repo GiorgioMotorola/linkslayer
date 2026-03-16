76770067201b23b81bdfe3ffc0074078cb84659a
Mike Weatherford <mikesweatherford@gmail.com>
2026-03-15 21:25:23 -0400

    fighter

--- src/utils/classes.js
+++ src/utils/classes.js
@@ -1,7 +1,7 @@
 export const classes = {
   Fighter: {
     name: "Fighter",
-    maxHP: 650,
+    maxHP: 65,
     special: "Power Strike",
     description:
       "65 HP. Class Ability(Power Strike) deals 8 damage. Starts with +1 Weapon Damage.",
@@ -11,12 +11,12 @@ export const classes = {
       { name: "Warlord's Strike", description: "Deals 18 damage and stuns the enemy." },
     ],
     specialEffect: (enemyHP) => enemyHP - 8,
-    startingWeaponBonus: 100,
+    startingWeaponBonus: 1,
     startingSpecialUses: 0,
-    startingShieldBonus: 100,
+    startingShieldBonus: 0,
     startingHealthPotionBonus: 0,
     startingInvisibilityCloaks: 0,
-    startingPlayerGold: 1000,
+    startingPlayerGold: 0,
   },
   Paladin: {
     name: "Paladin",
